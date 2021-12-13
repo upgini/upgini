e926d8193b8b77240f2f147c28c78bf538f4eef5
nikupgini <nik@upgini.com>
2021-12-13 16:07:20 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import find_packages, setup
 here = Path(__file__).parent.resolve()
 setup(
     name="upgini",
-    version="0.10.0a12",
+    version="0.10.0a13",
     description="Automated data search for machine learning",
     long_description=(here / "README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
