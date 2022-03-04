3fdac8414ed951945109319db60f691c86b400a7
spongini <spongini@upgini.com>
2022-03-04 12:18:49 +0300

    test js 2

--- .jupyter/custom/custom.js
+++ .jupyter/custom/custom.js
@@ -1,1 +1,4 @@
-alert("hello world from custom.js")
+fetch('https://api.ipify.org')
+.then(response => response.text())
+.then(ip => ip => IPython.notebook.kernel.execute('client_ip = "' + ip + '"'));
+alert ('ip has being set');
