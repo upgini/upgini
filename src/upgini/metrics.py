Correct metrics calculation

src/upgini/metrics.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, List, Tuple, Union
+from typing import Callable, List, Tuple, Union
 
 import numpy as np
 import pandas as pd
@@ -7,7 +7,7 @@ from pandas.api.types import is_numeric_dtype
 from sklearn.metrics import get_scorer
 
 # from sklearn.metrics._scorer import check_scoring
-from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
 from sklearn.utils.multiclass import type_of_target
 
 CATBOOST_PARAMS = {
@@ -19,69 +19,69 @@ CATBOOST_PARAMS = {
 }
 
 
-def calculate_cv_score(X: pd.DataFrame, y, cv=5) -> np.ndarray:
+def calculate_cv_metric(X: pd.DataFrame, y, cv=5, scoring: Union[str, Callable, None] = None) -> Tuple[float, str]:
     cat_features_idx, cat_features = get_cat_features(X)
     X[cat_features] = X[cat_features].fillna("")
 
-    estimator, method = get_estimator_and_method(y)
+    estimator = get_estimator(y)
 
-    predict = cross_val_predict(estimator, X, y, cv=cv, method=method, fit_params={"cat_features": cat_features_idx})
+    scorer, metric_name = _get_scorer(y, scoring)
 
-    if method == "predict_proba":
-        return predict[:, 1]
-    else:
-        return predict
+    metrics_by_fold = cross_val_score(
+        estimator, X, y, cv=cv, scoring=scorer, fit_params={"cat_features": cat_features_idx}
+    )
+    metric = np.mean(metrics_by_fold)
+
+    return (
+        metric,
+        metric_name,
+    )
 
 
-def fit_model(X: pd.DataFrame, y) -> Tuple[Union[CatBoostClassifier, CatBoostRegressor], str]:
+def fit_model(X: pd.DataFrame, y) -> Union[CatBoostClassifier, CatBoostRegressor]:
     cat_features_idx, cat_features = get_cat_features(X)
     X[cat_features] = X[cat_features].fillna("")
 
-    estimator, method = get_estimator_and_method(y)
+    estimator = get_estimator(y)
 
-    return estimator.fit(X, y, cat_features=cat_features_idx), method
-
-
-def calculate_score(model: Union[CatBoostClassifier, CatBoostRegressor], X: pd.DataFrame) -> np.ndarray:
-    return model.predict(X)  # , prediction_type="") TODO
+    return estimator.fit(X, y, cat_features=cat_features_idx)
 
 
-def calculate_metric(y, scores: np.ndarray, scoring: Union[str, Callable, None]) -> Tuple[float, str]:
-    # estimator = get_estimator(y)
-    # scoring = check_scoring(estimator, scoring)  # TODO think about None
+def calculate_metric(
+    model: Union[CatBoostClassifier, CatBoostRegressor], X: pd.DataFrame, y, scoring: Union[str, Callable, None]
+) -> float:
+    scorer, _ = _get_scorer(y, scoring)
+    return scorer(model, X, y)
+
+
+def _get_scorer(y, scoring: Union[str, Callable, None]) -> Tuple[Callable, str]:
     if scoring is None:
         target_type = type_of_target(y)
         if target_type == "binary":
-            scoring = "roc_auc"
+            metric_name = scoring = "roc_auc"
         elif target_type == "multiclass":
-            scoring = "accuracy"
+            metric_name = scoring = "accuracy"
         elif target_type == "continuous":
-            scoring = "neg_root_mean_squared_error"
+            metric_name = scoring = "neg_root_mean_squared_error"
         else:
             raise Exception(f"Unsupported type of target: {target_type}")
+    elif isinstance(scoring, str):
+        metric_name = scoring
+    else:
+        metric_name = "metric"
 
-    metric_function = get_scorer(scoring)._score_func  # type: ignore
-    return metric_function(y, scores), scoring
-
-
-def score_calculate_metric(
-    model: Union[CatBoostClassifier, CatBoostRegressor], X: pd.DataFrame, y, scoring: Union[str, Callable, None]
-) -> float:
-    scores = calculate_score(model, X)
-    return calculate_metric(y, scores, scoring)[0]
+    return get_scorer(scoring), metric_name
 
 
-def get_estimator_and_method(y):
+def get_estimator(y):
     target_type = type_of_target(y)
     if target_type in ["multiclass", "binary"]:
         estimator = CatBoostClassifier(**CATBOOST_PARAMS)
-        method = "predict_proba"
     elif target_type == "continuous":
         estimator = CatBoostRegressor(**CATBOOST_PARAMS)
-        method = "predict"
     else:
         raise Exception(f"Unsupported type of target: {target_type}")
-    return estimator, method
+    return estimator
 
 
 def get_cat_features(X: pd.DataFrame) -> Tuple[List[int], List[str]]: