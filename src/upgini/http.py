a1b7c737194d7025c56c9d884cff67b8cb3a391f
Marcel Coetzee <marcelcoetzee@tutanota.com>
2022-09-05 18:31:47 +0200

    Change variables to reflect SHA256 hash
    Signed-off-by: Marcel Coetzee <marcelcoetzee@tutanota.com>

--- src/upgini/http.py
+++ src/upgini/http.py
@@ -270,7 +270,7 @@ class _RestClient:
         api_path = self.INITIAL_SEARCH_URI_FMT_V2
 
         def open_and_send():
-            sha256_hash = hashlib.sha265()
+            sha256_hash = hashlib.sha256()
             with open(file_path, "rb") as file:
                 content = file.read()
                 sha256_hash.update(content)
@@ -345,7 +345,7 @@ class _RestClient:
         api_path = self.VALIDATION_SEARCH_URI_FMT_V2.format(initial_search_task_id)
 
         def open_and_send():
-            sha256_hash = hashlib.sha265()
+            sha256_hash = hashlib.sha256()
             with open(file_path, "rb") as file:
                 content = file.read()
                 sha256_hash.update(content)
