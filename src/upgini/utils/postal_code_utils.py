328f0a6f39f2ed8611a07b08f6a7d3e3d132e077
c3p0 <67713362+c3p0-upgini@users.noreply.github.com>
2022-11-12 14:52:55 +0300

    Hotfix check column names of search keys (#148)

--- src/upgini/utils/postal_code_utils.py
+++ src/upgini/utils/postal_code_utils.py
@@ -5,7 +5,7 @@ from upgini.utils.base_search_key_detector import BaseSearchKeyDetector
 
 class PostalCodeSearchKeyDetector(BaseSearchKeyDetector):
     def _is_search_key_by_name(self, column_name: str) -> bool:
-        return column_name.lower() in ["zip", "zipcode", "zip_code", "postal_code", "postalcode"]
+        return str(column_name).lower() in ["zip", "zipcode", "zip_code", "postal_code", "postalcode"]
 
     def _is_search_key_by_values(self, column: pd.Series) -> bool:
         return False
