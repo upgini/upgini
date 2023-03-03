4e6267df052c03277e8c80f6ce83e56fcda1b8b6
c3p0 <67713362+c3p0-upgini@users.noreply.github.com>
2023-03-03 19:32:46 +0300

    Check trial features only on transform (#189)

--- src/upgini/http.py
+++ src/upgini/http.py
@@ -182,10 +182,10 @@ class _RestClient:
     STOP_SEARCH_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/{0}/stop"
     SEARCH_MODELS_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/models/{0}"
     SEARCH_SCORES_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/scores/{0}"
-    SEARCH_FEATURES_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/rawfeatures/{0}"
+    SEARCH_FEATURES_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/rawfeatures/{0}?metricsCalculation={1}"
     SEARCH_MODEL_FILE_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/models/{0}/file"
     SEARCH_SCORES_FILE_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/scores/{0}/file"
-    SEARCH_FEATURES_FILE_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/rawfeatures/{0}/file"
+    SEARCH_FEATURES_FILE_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/rawfeatures/{0}/file?metricsCalculation={1}"
     SEARCH_FILE_METADATA_URI_FMT_V2 = SERVICE_ROOT_V2 + "search/{0}/metadata"
     SEARCH_TASK_METADATA_FMT_V3 = SERVICE_ROOT_V2 + "search/metadata-v2/{0}"
     SEARCH_DUMP_INPUT_FMT_V2 = SERVICE_ROOT_V2 + "search/dump-input"
@@ -509,12 +509,12 @@ class _RestClient:
         api_path = self.SEARCH_SCORES_FILE_URI_FMT_V2.format(ads_scores_id)
         return self._with_unauth_retry(lambda: self._send_get_file_req(api_path, trace_id))
 
-    def get_search_features_v2(self, trace_id: str, search_task_id: str):
-        api_path = self.SEARCH_FEATURES_URI_FMT_V2.format(search_task_id)
+    def get_search_features_v2(self, trace_id: str, search_task_id: str, metrics_calculation: bool):
+        api_path = self.SEARCH_FEATURES_URI_FMT_V2.format(search_task_id, str(metrics_calculation).lower())
         return self._with_unauth_retry(lambda: self._send_get_req(api_path, trace_id))
 
-    def get_search_features_file_v2(self, trace_id: str, ads_features_id: str):
-        api_path = self.SEARCH_FEATURES_FILE_URI_FMT_V2.format(ads_features_id)
+    def get_search_features_file_v2(self, trace_id: str, ads_features_id: str, metrics_calculation: bool):
+        api_path = self.SEARCH_FEATURES_FILE_URI_FMT_V2.format(ads_features_id, str(metrics_calculation).lower())
         return self._with_unauth_retry(lambda: self._send_get_file_req(api_path, trace_id))
 
     def upload_user_ads(self, file_path: str, metadata: FileMetadata):
