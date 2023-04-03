26bd12e74ea4a30bf4d0d32e5b886ab03060a437
c3p0-upgini <67713362+c3p0-upgini@users.noreply.github.com>
2023-04-03 16:27:04 +0400

    Handle transform without important features

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def send_log(msg: str):
 
 
 here = Path(__file__).parent.resolve()
-version = "1.1.108"
+version = "1.1.109"
 try:
     send_log(f"Start setup PyLib version {version}")
     setup(
