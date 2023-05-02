f063a799f47a34090002ac057ed350ba8e3a5c24
c3p0-upgini <67713362+c3p0-upgini@users.noreply.github.com>
2023-05-02 21:43:39 +0400

    Fix one-hot encoding check

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def send_log(msg: str):
 
 
 here = Path(__file__).parent.resolve()
-version = "1.1.123"
+version = "1.1.124"
 try:
     send_log(f"Start setup PyLib version {version}")
     setup(
