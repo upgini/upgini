4bf201a4caa4ca5494982085330db669d285d6e0
c3p0-upgini <67713362+c3p0-upgini@users.noreply.github.com>
2023-06-08 09:48:57 +0400

    Infer country by ip

--- src/upgini/utils/ip_utils.py
+++ src/upgini/utils/ip_utils.py
@@ -1,11 +1,12 @@
 import logging
 from typing import Dict, List, Optional
+
 import pandas as pd
 from requests import get
-from upgini.utils.track_info import get_track_metrics
-from upgini.resource_bundle import bundle
 
 from upgini.metadata import SearchKey
+from upgini.resource_bundle import bundle
+from upgini.utils.track_info import get_track_metrics
 
 
 class IpToCountrySearchKeyConverter:
@@ -30,20 +31,21 @@ class IpToCountrySearchKeyConverter:
             response = get(self.url.format(ip)).json()
             return response["countryCode"]
         except Exception:
-            return "US"
+            return None
 
     def convert(self, df: pd.DataFrame) -> pd.DataFrame:
         track_metrics = get_track_metrics()
-        if track_metrics is not None and "ip" in track_metrics and track_metrics["ip"] != "0.0.0.0":
+        if (
+            track_metrics is not None
+            and "ip" in track_metrics
+            and track_metrics["ip"] is not None
+            and track_metrics["ip"] != "0.0.0.0"
+        ):
             country_code = self._get_country_code(track_metrics["ip"])
             msg = bundle.get("country_auto_determined").format(country_code)
             print(msg)
             self.logger.info(msg)
             df["country_code"] = country_code
-        else:
-            msg = bundle.get("country_default_determined").format("US")
-            print(msg)
-            self.logger.info(msg)
-            df["country_code"] = "US"
-        self.search_keys["country_code"] = SearchKey.COUNTRY
+            self.search_keys["country_code"] = SearchKey.COUNTRY
+
         return df
