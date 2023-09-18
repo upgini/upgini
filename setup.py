379bfb9155cf94ccefece7b627a39e0097dc6fff
c3p0-upgini <67713362+c3p0-upgini@users.noreply.github.com>
2023-09-18 20:47:16 +0400

    Hotfix activating datasets

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def send_log(msg: str):
 
 
 here = Path(__file__).parent.resolve()
-version = "1.1.188"
+version = "1.1.189"
 try:
     send_log(f"Start setup PyLib version {version}")
     setup(
