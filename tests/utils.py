8961b0405061b4ea51fc111af0b60c7efe5db41e
c3p0-upgini <67713362+c3p0-upgini@users.noreply.github.com>
2023-10-17 09:01:51 +0400

    Fix tests

--- tests/utils.py
+++ tests/utils.py
@@ -23,6 +23,10 @@ def mock_default_requests(requests_mock: Mocker, url: str):
     requests_mock.post(url + "/private/api/v2/security/refresh_access_token", json={"access_token": "123"})
     requests_mock.get("https://pypi.python.org/pypi/upgini/json", json={"releases": {"1.0.0": [{}]}})
     requests_mock.post(url + "/public/api/v2/search/dump-input", content="123".encode())
+    requests_mock.get(
+        url + "/public/api/v2/user/transform-usage",
+        json={"transformedRows": 0, "restRows": 10000, "limit": 10000, "hasLimit": True},
+    )
 
 
 def random_id() -> str:
