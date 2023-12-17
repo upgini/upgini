593c3ce0469207d35d3526c99b9f203f8afaa699
upwindowship <lera@upgini.com>
2023-12-17 11:54:07 +0300

    Add AutoFE metadata classes

---
+++ src/upgini/autofe/vector.py
@@ -0,0 +1,21 @@
+from typing import List
+import pandas as pd
+from typing import List
+from upgini.autofe.operand import PandasOperand
+
+
+class Mean(PandasOperand):
+    name = "mean"
+    output_type = "float"
+    is_vector = True
+
+    def calculate_vector(self, data: List[pd.Series]) -> pd.Series:
+        return pd.DataFrame(data).T.fillna(0).mean(axis=1)
+
+
+class Sum(PandasOperand):
+    name = "sum"
+    is_vector = True
+
+    def calculate_vector(self, data: List[pd.Series]) -> pd.Series:
+        return pd.DataFrame(data).T.fillna(0).sum(axis=1)
