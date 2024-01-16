33310492fa1ee06c19f7b04bb07603cf21ac9bf7
upwindowship <142309150+upwindowship@users.noreply.github.com>
2024-01-16 14:55:23 +0300

    Truncate inexact operations (#260)
    * Truncate inexact operations
    
    * Fix codeowners
    
    * Add old account
    
    * Update README

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def send_log(msg: str):
 
 
 here = Path(__file__).parent.resolve()
-version = "1.1.237"
+version = "1.1.238"
 try:
     send_log(f"Start setup PyLib version {version}")
     setup(
