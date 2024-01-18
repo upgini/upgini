115827d2ea13e35de74a21c78be35f3050c86053
upwindowship <142309150+upwindowship@users.noreply.github.com>
2024-01-18 21:55:40 +0300

    Remove rows with empty target from groups (#262)
    * Remove rows with empty target from groups
    
    * Change groups column name
    
    * Don't pass groups in prepare_to_calculate
    
    * Fix pyproject
    
    * Fix tests + validate score column
    
    * Merge main
    
    ---------
    
    Co-authored-by: c3p0-upgini <67713362+c3p0-upgini@users.noreply.github.com>

--- src/upgini/features_enricher.py
+++ src/upgini/features_enricher.py
@@ -858,7 +858,7 @@ class FeaturesEnricher(TransformerMixin):
 
                 if X is not None and y is None:
                     raise ValidationError("X passed without y")
-                
+
                 effective_X = X if X is not None else self.X
                 effective_eval_set = eval_set if eval_set is not None else self.eval_set
 
@@ -2486,21 +2486,6 @@ class FeaturesEnricher(TransformerMixin):
             raise ValidationError(bundle.get("y_is_constant_eval_set"))
 
         return validated_eval_X, validated_eval_y
-    
-    def _validate_baseline_score(self, X: pd.DataFrame, eval_set: Optional[List[Tuple]]):
-        if self.baseline_score_column is not None:
-            if self.baseline_score_column not in X.columns:
-                raise ValidationError(bundle.get("baseline_score_column_not_exists").format(self.baseline_score_column))
-            if X[self.baseline_score_column].isna().any():
-                raise ValidationError(bundle.get("baseline_score_column_has_na"))
-            if eval_set is not None:
-                if isinstance(eval_set, tuple):
-                    eval_set = [eval_set]
-                for eval in eval_set:
-                    if self.baseline_score_column not in eval[0].columns:
-                        raise ValidationError(bundle.get("baseline_score_column_not_exists"))
-                    if eval[0][self.baseline_score_column].isna().any():
-                        raise ValidationError(bundle.get("baseline_score_column_has_na"))
 
     def _validate_baseline_score(self, X: pd.DataFrame, eval_set: Optional[List[Tuple]]):
         if self.baseline_score_column is not None:
