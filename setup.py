5dcf8cebd00b6620467394524c985c0c6a3f96bd
upwindowship <lera@upgini.com>
2024-02-07 19:15:06 +0300

    Correct version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def send_log(msg: str):
 
 
 here = Path(__file__).parent.resolve()
-version = "1.1.247"
+version = "1.1.246"
 try:
     send_log(f"Start setup PyLib version {version}")
     setup(
