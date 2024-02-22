15f080c0221aca2e3efed3be73491cfb72316731
c3p0upgini <155189648+c3p0upgini@users.noreply.github.com>
2024-02-22 19:56:25 +0400

    Bump version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def send_log(msg: str):
 
 
 here = Path(__file__).parent.resolve()
-version = "1.1.255a3233-2"
+version = "1.1.255a3233-3"
 try:
     send_log(f"Start setup PyLib version {version}")
     setup(
