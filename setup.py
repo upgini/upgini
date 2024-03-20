88c0c6235c8d353ab8c0a73fcd487730f755638f
upwindowship <142309150+upwindowship@users.noreply.github.com>
2024-03-20 12:26:03 +0300

    Increase imbalanced threshold (#272)
    * Increase imbalanced threshold
    
    * Bump version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def send_log(msg: str):
 
 
 here = Path(__file__).parent.resolve()
-version = "1.1.261"
+version = "1.1.262"
 try:
     send_log(f"Start setup PyLib version {version}")
     setup(
