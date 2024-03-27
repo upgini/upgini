Fix PSI calculation

tests/test_features_enricher.py
@@ -428,13 +428,13 @@ def test_saved_features_enricher(requests_mock: Mocker):
     df.drop(columns="SystemRecordId_473310000", inplace=True)
     train_df = df.head(10000)
     train_features = train_df.drop(columns="target")
-    train_target = train_df["target"]
+    train_target = train_df["target"].copy()
     eval1_df = df[10000:11000].reset_index(drop=True)
     eval1_features = eval1_df.drop(columns="target")
-    eval1_target = eval1_df["target"].reset_index(drop=True)
+    eval1_target = eval1_df["target"].reset_index(drop=True).copy()
     eval2_df = df[11000:12000]
     eval2_features = eval2_df.drop(columns="target")
-    eval2_target = eval2_df["target"]
+    eval2_target = eval2_df["target"].copy()
 
     enricher = FeaturesEnricher(
         search_keys={"phone_num": SearchKey.PHONE, "rep_date": SearchKey.DATE},
@@ -481,6 +481,31 @@ def test_saved_features_enricher(requests_mock: Mocker):
     first_feature_info = enricher.features_info.iloc[0]
     assert first_feature_info[feature_name_header] == "feature"
     assert first_feature_info[shap_value_header] == 10.1
+
+    # Check imbalanced target metrics
+    random = np.random.RandomState(42)
+    train_random_indices = random.choice(train_target.index, size=9000, replace=False)
+    train_target.loc[train_random_indices] = 0
+
+    metrics = enricher.calculate_metrics(
+        train_features,
+        train_target
+    )
+    expected_metrics = pd.DataFrame(
+        {
+            segment_header: [train_segment],
+            rows_header: [10000],
+            target_mean_header: [0.049],
+            enriched_gini: [0.000985],
+        }
+    )
+    print("Expected metrics: ")
+    print(expected_metrics)
+    print("Actual metrics: ")
+    print(metrics)
+
+    assert metrics is not None
+    assert_frame_equal(expected_metrics, metrics, atol=1e-6)
 
 
 def test_features_enricher_with_demo_key(requests_mock: Mocker):

tests/test_target_utils.py
@@ -135,7 +135,7 @@ def test_balance_undersaampling_multiclass():
     assert_frame_equal(balanced_df.sort_values(by=SYSTEM_RECORD_ID).reset_index(drop=True), expected_df)
 
 
-def test_psi_calculation():
+def test_binary_psi_calculation():
     df = pd.DataFrame({
         "target": [0, 0, 0, 0, 0, 1, 1, 1, 1, 1,   0, 0, 0, 0, 0, 1, 0, 1, 0, 1]
     })
@@ -169,3 +169,26 @@ def test_psi_calculation():
     enricher = FeaturesEnricher(search_keys={"date": SearchKey.DATE})
     enricher._validate_PSI(df)
     assert enricher.warning_counter._count == 2
+
+
+def test_regression_psi_calculation():
+    random = np.random.RandomState(42)
+    df = pd.DataFrame({
+        "target": random.rand(20)
+    })
+    df["date"] = pd.date_range("2020-01-01", "2020-01-20")
+    enricher = FeaturesEnricher(search_keys={"date": SearchKey.DATE})
+    enricher._validate_PSI(df)
+    assert enricher.warning_counter._count == 1
+
+    values1 = random.rand(10)
+    values2 = values1.copy()
+    values2[0] = 0.0
+    values2[9] = 1.0
+    df = pd.DataFrame({
+        "target": list(values1) + list(values2)
+    })
+    df["date"] = pd.date_range("2020-01-01", "2020-01-20")
+    enricher = FeaturesEnricher(search_keys={"date": SearchKey.DATE})
+    enricher._validate_PSI(df)
+    assert not enricher.warning_counter.has_warnings()