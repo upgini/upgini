f4ef6f131210f58779edb238128a0711d699247b
c3p0upgini <155189648+c3p0upgini@users.noreply.github.com>
2024-04-29 10:15:20 +0400

    Hotfix dates parsing warnings

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def send_log(msg: str):
 
 
 here = Path(__file__).parent.resolve()
-version = "1.1.276"
+version = "1.1.277"
 try:
     send_log(f"Start setup PyLib version {version}")
     setup(
