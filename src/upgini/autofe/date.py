2fe0c98634e777fe71952a3740c58a823d1e9810
upwindowship <142309150+upwindowship@users.noreply.github.com>
2024-05-17 16:24:25 +0300

    Add date percentile (#283)
    * Enable hash for composite features
    
    * Add date percentile
    
    * Add date unit and enable parsing bounds from string
    
    * Pass params
    
    * Show all operand names
    
    * Flake
    
    * Fix metadata
    
    * Assume step == 100
    
    * Flake
    
    * Fix using step
    
    * Bump version

--- src/upgini/autofe/date.py
+++ src/upgini/autofe/date.py
@@ -1,9 +1,9 @@
-from typing import Any, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 import numpy as np
 import pandas as pd
 from pandas.core.arrays.timedeltas import TimedeltaArray
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from upgini.autofe.operand import PandasOperand
 
@@ -27,6 +27,17 @@ class DateDiff(PandasOperand, DateDiffMixin):
     is_binary = True
     has_symmetry_importance = True
 
+    def get_params(self) -> Dict[str, Optional[str]]:
+        res = super().get_params()
+        res.update(
+            {
+                "diff_unit": self.diff_unit,
+                "left_unit": self.left_unit,
+                "right_unit": self.right_unit,
+            }
+        )
+        return res
+
     def calculate_binary(self, left: pd.Series, right: pd.Series) -> pd.Series:
         left = self._convert_to_date(left, self.left_unit)
         right = self._convert_to_date(right, self.right_unit)
@@ -41,6 +52,17 @@ class DateDiffType2(PandasOperand, DateDiffMixin):
     name = "date_diff_type2"
     is_binary = True
     has_symmetry_importance = True
+
+    def get_params(self) -> Dict[str, Optional[str]]:
+        res = super().get_params()
+        res.update(
+            {
+                "diff_unit": self.diff_unit,
+                "left_unit": self.left_unit,
+                "right_unit": self.right_unit,
+            }
+        )
+        return res
 
     def calculate_binary(self, left: pd.Series, right: pd.Series) -> pd.Series:
         left = self._convert_to_date(left, self.left_unit)
@@ -63,6 +85,15 @@ class DateListDiff(PandasOperand, DateDiffMixin):
     is_binary = True
     has_symmetry_importance = True
     aggregation: str
+
+    def get_params(self) -> Dict[str, Optional[str]]:
+        res = super().get_params()
+        res.update(
+            {
+                "aggregation": self.aggregation,
+            }
+        )
+        return res
 
     def __init__(self, **data: Any) -> None:
         if "name" not in data:
@@ -116,3 +147,55 @@ class DateListDiffBounded(DateListDiff):
     def _agg(self, x):
         x = x[(x >= (self.lower_bound or -np.inf)) & (x < (self.upper_bound or np.inf))]
         return super()._agg(x)
+
+
+class DatePercentile(PandasOperand):
+    name = "date_per"
+    is_binary = True
+    output_type = "float"
+
+    date_unit: Optional[str] = None
+    zero_month: Optional[int]
+    zero_year: Optional[int]
+    zero_bounds: Optional[List[float]]
+    step: int = 30
+
+    def get_params(self) -> Dict[str, Optional[str]]:
+        res = super().get_params()
+        res.update(
+            {
+                "date_unit": self.date_unit,
+                "zero_month": self.zero_month,
+                "zero_year": self.zero_year,
+                "zero_bounds": self.zero_bounds,
+                "step": self.step,
+            }
+        )
+        return res
+
+    @validator("zero_bounds", pre=True)
+    def validate_bounds(cls, value):
+        if value is None or isinstance(value, list):
+            return value
+        elif isinstance(value, str):
+            return value[1:-1].split(", ")
+
+    def calculate_binary(self, left: pd.Series, right: pd.Series) -> pd.Series:
+        # Assuming that left is a date column, right is a feature column
+        left = pd.to_datetime(left, unit=self.date_unit)
+        months = left.dt.month
+        years = left.dt.year
+
+        month_diffs = 12 * (years - (self.zero_year or 0)) + (months - (self.zero_month or 0))
+        bounds = month_diffs.apply(
+            lambda d: np.array(self.zero_bounds if self.zero_bounds is not None else []) + d * self.step
+        )
+
+        return right.index.to_series().apply(lambda i: self.__perc(right[i], bounds[i]))
+
+    def __perc(self, f, bounds):
+        hit = np.where(f >= bounds)[0]
+        if hit.size > 0:
+            return np.max(hit) + 1
+        else:
+            return np.nan
--- tests/test_autofe_operands.py
+++ tests/test_autofe_operands.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pandas as pd
 from pandas.testing import assert_series_equal
 
-from upgini.autofe.date import DateDiff, DateDiffType2, DateListDiff, DateListDiffBounded
+from upgini.autofe.date import DateDiff, DateDiffType2, DateListDiff, DateListDiffBounded, DatePercentile
 
 
 def test_date_diff():
@@ -93,3 +93,32 @@ def test_date_diff_list_bounded():
     check_num_by_years(30, 45, "date_diff_Y_30_45_count", pd.Series([0, 1, 0, 0, 0]))
     check_num_by_years(45, 60, "date_diff_Y_45_60_count", pd.Series([0, 1, 0, 0, 0]))
     check_num_by_years(60, None, "date_diff_Y_60_plusinf_count", pd.Series([0, 1, 0, 0, 0]))
+
+
+def test_date_percentile():
+    data = pd.DataFrame(
+        [
+            ["2024-03-03", 2],
+            ["2024-02-03", 2],
+            ["2024-02-04", 34],
+            ["2024-02-05", 32],
+            ["2023-03-03", 60],
+            ["2023-03-02", None],
+        ],
+        columns=["date", "feature"],
+    )
+    operand = DatePercentile(
+        zero_month=2,
+        zero_year=2024,
+        zero_bounds="[0.0, 2.6, 3.2, 3.8, 4.4, 5.0, 5.6, 6.2, 6.8, 7.3999999999999995, 8.0, 8.6, 9.2, "
+        "9.8, 10.4, 11.0, 11.6, 12.200000000000001, 12.799999999999999, 13.4, 14.0, 14.6, 15.2, 15.8, 16.4, 17.0,"
+        " 17.6, 18.200000000000003, 18.8, 19.4, 20.0, 20.6, 21.200000000000003, 21.8, 22.400000000000002, 23.0, 23.6,"
+        " 24.2, 24.8, 25.4, 26.0, 26.599999999999998, 27.2, 27.8, 28.4, 29.0, 29.6, 30.2, 30.799999999999997, 31.4,"
+        " 32.0, 32.04, 32.08, 32.12, 32.16, 32.2, 32.24, 32.28, 32.32, 32.36, 32.4, 32.44, 32.48, 32.52, 32.56, 32.6, "
+        "32.64, 32.68, 32.72, 32.76, 32.8, 32.84, 32.88, 32.92, 32.96, 33.0, 33.04, 33.08, 33.12, 33.16, 33.2, 33.24,"
+        " 33.28, 33.32, 33.36, 33.4, 33.44, 33.48, 33.52, 33.56, 33.6, 33.64, 33.68, 33.72, 33.76, 33.8, 33.84, 33.88,"
+        " 33.92, 33.96]",
+    )
+
+    expected_values = pd.Series([None, 1, 100, 51, 100, None])
+    assert_series_equal(operand.calculate(left=data.date, right=data.feature), expected_values)
