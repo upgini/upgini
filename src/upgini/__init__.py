18342f188b1db28a1aec6373a6d5a241b93d3f1e
c3p0upgini <155189648+c3p0upgini@users.noreply.github.com>
2024-09-06 14:17:23 +0400

    Fix dependencies

--- src/upgini/__init__.py
+++ src/upgini/__init__.py
@@ -1,11 +1,13 @@
 import os
 
-from .lazy_import import LazyImport
+from upgini.features_enricher import FeaturesEnricher  # noqa: F401
+from upgini.metadata import SearchKey, CVType, RuntimeParameters, ModelTaskType  # noqa: F401
+# from .lazy_import import LazyImport
 
 os.environ["SETUPTOOLS_USE_DISTUTILS"] = "stdlib"
 
-FeaturesEnricher = LazyImport("upgini.features_enricher", "FeaturesEnricher")
-SearchKey = LazyImport("upgini.metadata", "SearchKey")
-RuntimeParameters = LazyImport("upgini.metadata", "RuntimeParameters")
-CVType = LazyImport("upgini.metadata", "CVType")
-ModelTaskType = LazyImport("upgini.metadata", "ModelTaskType")
+# FeaturesEnricher = LazyImport("upgini.features_enricher", "FeaturesEnricher")
+# SearchKey = LazyImport("upgini.metadata", "SearchKey")
+# RuntimeParameters = LazyImport("upgini.metadata", "RuntimeParameters")
+# CVType = LazyImport("upgini.metadata", "CVType")
+# ModelTaskType = LazyImport("upgini.metadata", "ModelTaskType")
