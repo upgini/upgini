6d95dc826c7ab867d3e67461b1156ad4b80d86a3
upwindowship <lera@upgini.com>
2024-11-25 20:27:05 +0300

    Import operands

--- src/upgini/autofe/all_operands.py
+++ src/upgini/autofe/all_operands.py
@@ -1,4 +1,9 @@
 from upgini.autofe.operand import OperandRegistry
+from upgini.autofe.binary import *  # noqa
+from upgini.autofe.unary import *  # noqa
+from upgini.autofe.groupby import *  # noqa
+from upgini.autofe.vector import *  # noqa
+from upgini.autofe.date import *  # noqa
 
 
 def find_op(name):
