94848717baa403912b335d122e2c09c8edc4b39a
upwindowship <lera@upgini.com>
2025-04-08 17:50:26 +0300

    Add vectorize operator

--- src/upgini/autofe/vector.py
+++ src/upgini/autofe/vector.py
@@ -22,3 +22,12 @@ class Sum(PandasOperator, VectorizableMixin):
 
     def calculate_vector(self, data: List[pd.Series]) -> pd.Series:
         return pd.DataFrame(data).T.fillna(0).sum(axis=1)
+
+
+class Vectorize(PandasOperator, VectorizableMixin):
+    name: str = "vectorize"
+    is_vector: bool = True
+    group_index: int = 0
+
+    def calculate_vector(self, data: List[pd.Series]) -> pd.Series:
+        return pd.DataFrame(data).T.apply(lambda x: x.to_list(), axis=1)
