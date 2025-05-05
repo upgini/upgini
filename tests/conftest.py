14a8d457d4141d02375b1f08921dd448d3b0993c
upwindowship <lera@upgini.com>
2025-05-05 20:02:55 +0300

    Return flag

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,7 +20,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture
 def update_metrics_flag(request):
-    return request.config.getoption("--update-metrics") or True
+    return request.config.getoption("--update-metrics") or False
 
 
 @pytest.fixture
