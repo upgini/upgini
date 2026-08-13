403e4448e6e4b2d8fbaa5c16cb82a0cc818bd606
sunmarinup <vdymbitskaya@upgini.com>
2026-08-13 15:05:49 +0300

    Fix PSI comparison

--- src/upgini/features_enricher.py
+++ src/upgini/features_enricher.py
@@ -1578,8 +1578,6 @@ class FeaturesEnricher(TransformerMixin):
         psi_values = calculate_features_psi(
             psi_df, cat_features, date_column, self.logger, model_task_type, stability_agg_func
         )
-        if baseline_score_column:
-            psi_values[baseline_score_column] = None
 
         self.logger.info(f"PSI values by value: {psi_values}")
 
@@ -1594,6 +1592,7 @@ class FeaturesEnricher(TransformerMixin):
         total_unstable_features = sorted(set(unstable_by_sparsity + unstable_by_value))
         if baseline_score_column:
             total_unstable_features = [f for f in total_unstable_features if f != baseline_score_column]
+            self.psi_values[baseline_score_column] = None
 
         return total_unstable_features
 
